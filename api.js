56fc20188704636d650a3f62c0c9ea9f4b06fc05
kennyosc <kennyoscar95@gmail.com>
2019-07-09 10:32:07 +0700

    add get users using req.params

--- api.js
+++ api.js
@@ -50,7 +50,7 @@ MongoClient.connect(URL, {useNewUrlParser:true}, (err, client)=>{
     //FIND BY PARAMETERS
     app.get('/selecteduser',(req,res)=>{
 
-        //app.get -> mengambil get dari postman/input user dengan req.query
+        //app.get -> mengambil get dari postman/input user dengan REQ.QUERY
         const data_age = parseInt(req.query.age)
 
         db.collection('users').find({
@@ -63,9 +63,10 @@ MongoClient.connect(URL, {useNewUrlParser:true}, (err, client)=>{
     //GET USERS BY AGE AND MARRIED
     app.get('/agemarried',(req,res)=>{
 
-        //app.get -> mengambil get dari postman/input user dengan req.query
+        //app.get -> mengambil get dari postman/input user dengan REQ.QUERY
         const data_age = parseInt(req.query.age)
-        const data_married = (req.query.married == 'true')
+        const data_married = (req.query.married === 'true')
+        //bisa juga const data_married = JSON.parse(req.query.married) --> tpi kalau diisi FALSE(dengan huruf kapital, itu tidak bisa)
 
         db.collection('users').find({
             age: {$gt:data_age}, 
@@ -78,7 +79,7 @@ MongoClient.connect(URL, {useNewUrlParser:true}, (err, client)=>{
     //GET USERS BY RANGE OF AGE
     app.get('/agerange',(req,res)=>{
 
-        //app.get -> mengambil get dari postman/input user dengan req.query
+        //app.get -> mengambil get dari postman/input user dengan REQ.QUERY
         const range_bawah = parseInt(req.query.bawah)
         const range_atas = parseInt(req.query.atas)
 
@@ -89,10 +90,22 @@ MongoClient.connect(URL, {useNewUrlParser:true}, (err, client)=>{
         })
     })
 
+    //GET USERS MENGGUNAKAN REQ.PARAMS
+    app.get('/params/:age',(req,res)=>{
+        const age = parseInt(req.params.age)
+
+        db.collection('users').find({age:{$gt:age}}).toArray().then((results)=>{
+            res.send({
+                message: 'Data berhasil didapatkan',
+                age: results
+            })
+        })
+    })
+
     //POST 1 DATA
     app.post('/inputsatudata', (req,res)=>{
         //ambil data dari user database
-        // apa yang di post di UI oleh user, bisa diambil dengan req.body
+        // apa yang di post di UI oleh user, bisa diambil dengan REQ.BODY
         // console.log(req.body)
 
         //app.post -> mengambil datanya dengan req.body
@@ -107,8 +120,10 @@ MongoClient.connect(URL, {useNewUrlParser:true}, (err, client)=>{
             age: data_age,
             married: data_married
         }).then((result)=>{
-            res.send('Data berhasil ditambahkan')
-            console.log(result)
+            //isinya result.ops adalah data yang kita post dalam bentuk array [{.....}]
+            res.send(result.ops)
+            //results.ops itu khusus untuk post, kalau GET itu hanya results
+            console.log(result.ops)
         })
     })    
 
@@ -118,7 +133,8 @@ MongoClient.connect(URL, {useNewUrlParser:true}, (err, client)=>{
         const data = req.body
 
         db.collection('users').insertMany(data).then(results=>{
-            res.send(results)
+            //results.ops itu khusus untuk post, kalau GET itu hanya results
+            res.send(results.ops)
         })
     })
 
